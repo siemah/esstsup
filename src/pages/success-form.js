7da63b773ec7f15e3e5d4356ffb6e248644e7f27
siemah <m_hamiche@inptic.edu.dz>
2020-01-09 23:13:01 +0100

    fix: success form

--- src/pages/success-form.js
+++ src/pages/success-form.js
@@ -1,45 +1,41 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import BlockIcon from "../components/home/BlockIcon";
 
 import '../assets/css/home.css';
 
-import RowBlock from "../components/home/RowBlock";
 import { Row, Container, Col } from "../components/layout/grid";
-import Card from "../components/home/Card";
-import { faChalkboardTeacher, faBookReader, faBrain, faAtom, faGlobe, faBuilding, faPhone, faEnvelope, faMobile, faMap, faMapMarker, faMapMarkerAlt, faPlus, faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
-import HomeHero from "../components/home/HomeHero";
+import { Link } from "gatsby";
 
-import TelecomIcon from '../assets/images/icons/telecom.svg';
-import InfoIcon from '../assets/images/icons/info.svg';
-import ElectroIcon from '../assets/images/icons/electro.svg';
-import ChimieIcon from '../assets/images/icons/chimie.svg';
-import Logo from '../assets/images/esst-logo.png';
-import RegisterForm from "../components/RegisterForm";
-import Gallery from "../components/home/Gallery";
-import Carousel from "../components/Carousel";
-import Testiminilas from "../components/home/Testiminilas";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import Link from "gatsby-plugin-transition-link/Link";
 
-const IndexPage = ({ data }) => {
+const SuccessFormPage = () => {
 
   return (
     <Layout>
-
-      <Container>
-        <Row>
-          <Col s={12}>
-            <h1>
-              Message envoye avec success
+      <SEO title={`Message de success`} />
+      <div style={{
+        padding: '5rem 0'
+      }}>
+        <Container className={`center`}>
+          <Row>
+            <Col s={12}>
+              <h1>
+                Message envoye avec success
             </h1>
-          </Col>
-        </Row>
-      </Container>
-
+              <div>
+                <Link to={`/`} style={{
+                  color: '#ffffff',
+                  padding: '1rem',
+                  fontWeight: 'bold'
+                }} className={`card__link-ui bg-blue`}>
+                  Retour vers la page d'accueil
+              </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Layout>
   );
 }
