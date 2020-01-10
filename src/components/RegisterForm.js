b42ca08d70fc6954c47c463e0d002242161bcc34
siemah <m_hamiche@inptic.edu.dz>
2020-01-10 02:43:53 +0100

    build: add other contacts source

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -4,10 +4,18 @@ import { FormField, Button } from './layout/form';
 
 import '../assets/css/register-form.css';
 
-export default function RegisterForm({ className=''}) {
+export default function RegisterForm({ className='', title=null}) {
   return (
     <form className={`register-form__block ${className}`} netlify-honeypot="bot-field" action='/success-form/' name="formulaire-de-contact" method="POST" data-netlify="true">
       <input type="hidden" name="form-name" value="formulaire-de-contact" />
+      {
+        title &&
+        <Row>
+          <Col s={12}>
+            <h2 className={`form__legend-title mb0`}>{title}</h2>
+          </Col>
+        </Row>
+      }
       <Row className="mb0 js-slide-from-down" data-delay={0.1}>
         <Col s='12'>
           <FormField name='nom-complet' placeholder='Nom Complet' />
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ const ContactPage = ({ data }) => {
       <SEO title={`Contact`} />
       <div className={`fake-block`} />
       <div className='contact-page'>
-        
+
         <div className={`map-form__block flex-block`}>
           <div className='map-block'>
             <Map
@@ -28,14 +28,34 @@ const ContactPage = ({ data }) => {
               label="Siège de l'esst" />
           </div>
           <div className={`form-block`}>
-            <RegisterForm />
+            <RegisterForm title={'Contact'} />
           </div>
         </div>
 
-        <Container>
+        <Container className={`pt2`}>
           <Row>
-            <Col>
-              
+            <Col s={12} m={6}>
+              <h4 className={`mb1`}>Contactez nous</h4>
+              <ul className="list-block m0 p0 mb1">
+                <li className="lite-item mb0 opacity-0 js-slide-from-down">
+                  Par Téléphone: <a href="tel:0213799931699" className='contact-link'>(0) 7 99 93 16 99</a> ou{` `}
+                  <a href="tel:021323300801" className='contact-link'>(0) 23 30 08 01</a>
+                </li>
+                <li className="lite-item mb0 opacity-0 js-slide-from-down">
+                  Par Email: <a href="mailto:contact@esst-sup.com" className='contact-link'>contact@esst-sup.com</a>
+                </li>
+              </ul>
+              </Col>
+              <Col s={12} m={6}>
+              <h4 className={`mb1`}>Vous pouvez nous trouver</h4>
+              <ul>
+                <li className="lite-item mb0 opacity-0 js-slide-from-down">
+                  Sur Facebook: <a href="https://www.facebook.com/esst-sup" className='contact-link'>ESSTSUP</a>
+                </li>
+                <li className="lite-item mb0 opacity-0 js-slide-from-down">
+                  Sur Instagram: <a href="https://www.instagram.com/esst_sup/" className='contact-link'>ESST_SUP</a>
+                </li>
+              </ul>
             </Col>
           </Row>
         </Container>
